dc22b5bff3350d5dadb40890e8ab1fa779ead930
zanoone <zanoone2@gmail.com>
2025-11-05 18:27:58 +0900

    Change bundle ID from com.sefra to kr.sefra
    - Update Android applicationId to kr.sefra
    - Update package name in MainActivity
    - Update codemagic.yaml BUNDLE_ID

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.sefra"
+    namespace = "kr.sefra"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.sefra"
+        applicationId = "kr.sefra"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/kr/sefra/MainActivity.kt
+++ android/app/src/main/kotlin/kr/sefra/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.sefra.sefra_flutter
+package kr.sefra
 
 import io.flutter.embedding.android.FlutterActivity
 
